cd51ac5398c9d780a05c3caae1fd46ea57ad527b
1999fych <fych1998@foxmail.com>
2017-12-21 16:55:11 +0800

    my first change

---
+++ fych.js
@@ -0,0 +1,12 @@
+//console.log('我的第一个node')
+var hello='hello node';
+const sliceNode=hello.slice(5)
+console.log(sliceNode)//node
+
+var hello='hello node';
+const str=hello.substr(5,8)//返回开始长度
+console.log(str)//node
+
+var hello='hello node';
+const str_substring=hello.substring(5,8)//返回开始长度
+console.log(str)//node
